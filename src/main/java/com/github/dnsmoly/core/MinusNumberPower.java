b8cb6200bc543f2a73749adc0971ad04d413a075
dnsmoly <dn.smoly@gmail.com>
2025-01-14 11:52:04 +0300

    feat: negative number power

---
+++ src/main/java/com/github/dnsmoly/core/MinusNumberPower.java
@@ -0,0 +1,8 @@
+package com.github.dnsmoly.core;
+
+public class MinusNumberPower {
+    public static void main(String[] args) {
+        var x =Integer.valueOf(-3);
+        System.out.println("-3 ^ 2 = " + (Math.pow(x, 2)));
+    }
+}
